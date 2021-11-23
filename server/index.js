bf7cc05de860c6782425cf0ad74315f8bf7f8048
miketolcer <miketolcer@yahoo.com>
2021-11-23 17:20:26 -0500

    2nd commit

--- server/index.js
+++ server/index.js
@@ -35,6 +35,53 @@ app.get("/api/fortune", (req, res) => {
   res.status(200).send(randomFortune);
 
 });
+let nameArry = []
+app.post("/api/nameForm", (req, res) => {
+  const { name, email} = req.body
+  nameArry.push(req.body)
+  console.log(req.body)
+  res.sendStatus(200)
+})
+
+let users = [
+  { name: "Mike", email: "mike@gmail.com" },
+  { name: "David", email: "david@gmail.com" },
+];
+
+let changedUser = []
+app.put("/api/userArray/:name1",(req,res) => {
+  const {name1, newEmail} = req.body
+
+  
+  for (let i =0; i< users.length; i++) {
+      let currentUser = users[i];
+      if (currentUser.name === name1) {
+          currentUser.email = newEmail
+      }
+      if (currentUser.name !== name1) {
+          users.push({name: name1, email: newEmail})
+      }
+  }
+  // changedUser.push(req.body)  
+  // console.log(req.body)
+  res.sendStatus(200)
+})
+
+app.get("/api/horoscope", (req,res) => {
+  const horoscopes = ['Power down for a minute and reassess your strategy.',
+  'Does your overall plan of attack need a serious revamp?',
+  'Just keep an eye on that work/life balance during this busy month.',
+  'Have your big swings resulted in some big misses recently?',
+  'Would new software and apps increase your productivity?'
+  ];
+
+  let randomIndex = Math.floor(Math.random() * horoscopes.length);
+  let randomHoroscope = horoscopes[randomIndex];
+
+  res.status(200).send(randomHoroscope);
+})
+
+
 
 
 app.listen(4000, () => console.log("Server running on 4000"));
